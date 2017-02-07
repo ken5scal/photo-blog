7aaf1ccbbea960faf6bf489cc7036d107557c4c5
ken5scal <kengoscal@gmail.com>
2017-02-07 09:21:07 +0900

    Appen values in cookie

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"net/http"
 	"github.com/satori/go.uuid"
+	"strings"
 )
 
 var tmpl *template.Template
@@ -20,8 +21,11 @@ func main() {
 
 func index(res http.ResponseWriter, req *http.Request) {
 	cookie := getCookie(res, req)
-	tmpl.ExecuteTemplate(res, "index.html", cookie)
+	cookie = appendCookieValue(cookie, res)
+	xs := strings.Split(cookie.Value, "|")
+	tmpl.ExecuteTemplate(res, "index.html", xs)
 }
+
 func getCookie(res http.ResponseWriter, request *http.Request) *http.Cookie {
 	cookie, err := request.Cookie("session")
 	if err != nil {
@@ -33,5 +37,25 @@ func getCookie(res http.ResponseWriter, request *http.Request) *http.Cookie {
 		http.SetCookie(res, cookie)
 	}
 
+	return cookie
+}
+
+func appendCookieValue(cookie *http.Cookie, res http.ResponseWriter) *http.Cookie {
+	p1 := "dog.jpg"
+	p2 := "cat.jpg"
+	p3 := "rabbit.jpg"
+
+	value := cookie.Value
+	if !strings.Contains(value, p1) {
+		value += "|" + p1
+	}
+	if !strings.Contains(value, p2) {
+		value += "|" + p2
+	}
+	if !strings.Contains(value, p3) {
+		value += "|" + p3
+	}
+	cookie.Value = value
+	http.SetCookie(res, cookie)
 	return cookie
 }
