fba5ef659f5e952f6a79e4d666640cb545663574
ken5scal <kengoscal@gmail.com>
2017-02-07 09:06:08 +0900

    Just another template

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
-package photo_blog
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tmpl *template.Template
+
+func init() {
+	tmpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(res http.ResponseWriter, req *http.Request) {
+	tmpl.ExecuteTemplate(res, "index.html", nil)
+}
